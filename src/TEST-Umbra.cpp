157e1373a28abc2c6e6dd1724706e03f1f74d443
UnSchtalch <unschtalch@unschtalch-netbook.(none)>
2012-11-23 13:36:08 +0400

    Sources created

---
+++ src/TEST-Umbra.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TEST-Umbra.cpp
+// Author      : UnSchtalch
+// Version     :
+// Copyright   : Free to use
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
