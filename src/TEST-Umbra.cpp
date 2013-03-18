785101a02a2d9ba7f3ec898737afde09803df0fd
UnSchtalch <shadow.93.04@gmail.com>
2013-03-18 11:22:25 +0400

    Started OGL window
    Wrap source with eclipse project files, initialised OGL window

--- src/TEST-Umbra.cpp
+++ src/TEST-Umbra.cpp
@@ -7,9 +7,37 @@
 //============================================================================
 
 #include <iostream>
+#include <GL/gl.h>
+#include <GL/freeglut.h>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+void
+init(void)
+
+{
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+void
+display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glFlush();
+}
+
+int
+main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(800, 600);
+  glutInitWindowPosition(200, 100);
+  glutCreateWindow("Umbra");
+  init();
+  glutDisplayFunc(display);
+  glutMainLoop();
+  return 0;
 }
