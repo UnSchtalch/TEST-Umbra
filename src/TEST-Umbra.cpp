196c9b4e3c05b992c6ab2321b0d841131548d952
UnSchtalch <shadow.93.04@gmail.com>
2013-03-22 20:28:13 +0400

    Fixed PNG loading
    Now Kona-chan works even under Windows!

--- src/TEST-Umbra.cpp
+++ src/TEST-Umbra.cpp
@@ -14,10 +14,9 @@ using namespace std;
 bool loadPNGImage(char* name, int &outWidth, int &outHeght, bool &outHasAlpha,
 		GLubyte **outData);
 
-GLubyte *textureImage;//bad idea, just for experiment;
+GLubyte *textureImage;  //bad idea, just for experiment;
 
-GLuint
-png_texture(char *filename)
+GLuint png_texture(char *filename)
 {
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
@@ -34,8 +33,13 @@ png_texture(char *filename)
 	bool hasAlpha;
 	bool success = false;
 	success = loadPNGImage(filename, width, height, hasAlpha, &textureImage);
-	if (!success) return 0;
-	glTexImage2D(GL_TEXTURE_2D,0,hasAlpha ? 4:3, width, height,0,hasAlpha ? GLUT_RGBA:GL_RGB, GL_UNSIGNED_BYTE, textureImage);
+	if (!success)
+	{
+		cout << "fail!" << endl;
+		return 0;
+	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+			GL_UNSIGNED_BYTE, textureImage);
 	return tex;
 }
 
@@ -43,15 +47,18 @@ GLuint textnum;
 
 void display(void)
 {
+	textnum = png_texture("./1.png");
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBindTexture(GL_TEXTURE_2D, textnum);
 	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_QUADS); {
-		glTexCoord2f(0.0, 0.0); glVertex2f(0.0, 0.0);
+	glBegin(GL_QUADS);
+	{
+		glTexCoord2f(0.0, 0.0);  glVertex2f(0.0, 0.0);
 		glTexCoord2f(1.0, 0.0); glVertex2f(1.0, 0.0);
 		glTexCoord2f(1.0, 1.0); glVertex2f(1.0, 1.0);
 		glTexCoord2f(0.0, 1.0); glVertex2f(0.0, 1.0);
-	} glEnd();
+	}
+	glEnd();
 
 	glFlush();
 
@@ -61,13 +68,14 @@ void init(void)
 
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glEnable(GL_BLEND);//transparency
+	glEnable(GL_BLEND);  //transparency
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	cout<<glGetError()<<endl;
 	glEnable(GL_TEXTURE_2D);
-	textnum = png_texture("monkey.png");
+	cout<<glGetError()<<endl;
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(-0.1, 1.1, -0.1, 1.1);
+	//gluOrtho2D(-0.1, 1.1, -0.1, 1.1);
 
 }
 
